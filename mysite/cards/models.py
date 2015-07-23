792fa7806e5e00f81944a6cf749afdc1281b2be6
Ema Chiroiu <chiroiuemilia@gmail.com>
2015-07-23 10:53:00 +0300

    Second app Cards created

--- mysite/cards/models.py
+++ mysite/cards/models.py
@@ -1,8 +1,9 @@
 from django.db import models
-
-# Create your models here.
 class Card(models.Model):
 	name = models.CharField(max_length=60)
 	media_type=models.CharField(max_length=32)
 	image= models.ImageField()
 	url= models.URLField(max_length=200)
+
+
+# Create your models here.
