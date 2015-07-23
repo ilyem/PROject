67531273961190d29b0a63e3f9e060ee38e0dc5b
Ema Chiroiu <chiroiuemilia@gmail.com>
2015-07-23 13:29:07 +0300

    3rd edit

--- mysite/cards/models.py
+++ mysite/cards/models.py
@@ -1,9 +1,17 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+import datetime
 class Card(models.Model):
+	owner= models.ForeignKey(User)
 	name = models.CharField(max_length=60)
 	media_type=models.CharField(max_length=32)
 	image= models.ImageField()
 	url= models.URLField(max_length=200)
+	created_at= models.DateTimeField()
+
+	def __str__(self):
+		return self.name
 
 
 # Create your models here.
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
+
+
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-]
+    url(r'^polls/', include('polls.urls')),
+] +static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
